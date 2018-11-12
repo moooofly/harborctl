fb18d74681b891b38013153c2a7cbcc0dcf54621
moooofly <centos.sf@gmail.com>
2018-11-12 13:35:13 +0800

    refactor(tool): update check.sh to get addresses by ENV variables

--- tools/sync_status_check/check.sh
+++ tools/sync_status_check/check.sh
@@ -2,7 +2,7 @@
 
 
 help() {
-    echo "Usage: USER=<user> PASS=<passord> ./check.sh <repo> <tag>"
+    echo "Usage: USER=<user> PASS=<passord> STAG=<stag_harbor_addr> PROD=<prod_harbor_addr> ./check.sh <repo> <tag>"
     exit 0
 }
 
@@ -10,26 +10,23 @@ if [ $# -ne 2 ] ; then
     help
 fi
 
-user=$USER
-pass=$PASS
-
 repo=$1
 tag=$2
 
-./harborctl login -u $user  -p $pass --address stag-reg.llsops.com
+./harborctl login -u $USER  -p $PASS --address $STAG
 echo "-----"
-#./harborctl job replication list -i 7 --status error -r $1 --address stag-reg.llsops.com
+#./harborctl job replication list -i 7 --status error -r $1 --address $STAG
 #echo "-----"
-./harborctl repository tag get -r $1 -t $2 --address stag-reg.llsops.com
+./harborctl repository tag get -r $1 -t $2 --address $STAG
 
 echo
 echo
 echo
 
 echo "-----"
-./harborctl login -u $user -p $pass --address prod-reg.llsops.com
+./harborctl login -u $USER -p $PASS --address $PROD
 echo "-----"
-./harborctl repository tag manifest -r $1 -t $2 -v v2 --address prod-reg.llsops.com
+./harborctl repository tag get -r $1 -t $2 --address $PROD
 echo "-----"
-./harborctl repository tag get -r $1 -t $2 --address prod-reg.llsops.com
+./harborctl repository tag manifest -r $1 -t $2 -v v2 --address $PROD
 echo "-----"
